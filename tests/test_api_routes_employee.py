f718083160e99104b740b6fea418a20b9ff36cef
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-04 18:23:46 +0100

    moved dummy employee in constant rather than setup prop

--- tests/test_api_routes_employee.py
+++ tests/test_api_routes_employee.py
@@ -6,43 +6,27 @@ from app.api.routes.employee import create_employee, get_employee
 from app.repository.employee import EmployeeRepository
 from app.schema.employee import Employee, EmployeeCreate
 
+DUMMY_EMPLOYEE = Employee(
+    id=UUID("00000000-0000-0000-0000-000000000001"),
+    first_name="John",
+    last_name="Doe",
+)
+
 
 class TestEmployeeIsolatedRoute(unittest.TestCase):
     def setUp(self):
         self.session = MagicMock()
         self.repository = EmployeeRepository
 
-        self.dummy_employee = Employee(
-            id=UUID("00000000-0000-0000-0000-000000000001"),
-            first_name="John",
-            last_name="Doe",
-        )
-
-    @patch.object(
-        EmployeeRepository,
-        "get",
-        return_value=Employee(
-            id=UUID("00000000-0000-0000-0000-000000000001"),
-            first_name="John",
-            last_name="Doe",
-        ),
-    )
+    @patch.object(EmployeeRepository, "get", return_value=DUMMY_EMPLOYEE)
     def test_get_employee(self, get: MagicMock):
-        response = get_employee(self.session, employee_id=self.dummy_employee.id)
-        get.assert_called_once_with(session=self.session, id=self.dummy_employee.id)
-        assert response == self.dummy_employee
+        response = get_employee(self.session, employee_id=DUMMY_EMPLOYEE.id)
+        get.assert_called_once_with(session=self.session, id=DUMMY_EMPLOYEE.id)
+        assert response == DUMMY_EMPLOYEE
 
-    @patch.object(
-        EmployeeRepository,
-        "create",
-        return_value=Employee(
-            id=UUID("00000000-0000-0000-0000-000000000001"),
-            first_name="John",
-            last_name="Doe",
-        ),
-    )
+    @patch.object(EmployeeRepository, "create", return_value=DUMMY_EMPLOYEE)
     def test_create_employee(self, create: MagicMock):
         employee = EmployeeCreate(first_name="John", last_name="Doe")
         response = create_employee(session=self.session, employee=employee)
         create.assert_called_once_with(session=self.session, obj_in=employee.dict())
-        assert response == self.dummy_employee
+        assert response == DUMMY_EMPLOYEE
