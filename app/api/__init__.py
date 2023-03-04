594e556d220027b98b42f0284bd6a9d0dfaf6484
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-04 15:30:24 +0100

    added type hint for app

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,8 @@
-from .routes import health, employee
+from fastapi import FastAPI
+
+from .routes import employee, health
 
 
-def add_app_routes(app):
+def add_app_routes(app: FastAPI):
     app.include_router(health.router, prefix="/health", tags=["Health"])
     app.include_router(employee.router, prefix="/employee", tags=["Employee"])
