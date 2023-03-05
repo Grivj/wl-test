6341b2ef2f969274eab6c6d62ef80d1bc653f962
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 21:12:50 +0100

    fixed broken test (need some work)

--- tests/test_api_routes_employee.py
+++ tests/test_api_routes_employee.py
@@ -5,11 +5,14 @@ from uuid import UUID
 from app.api.routes.employee import create_employee
 from app.repository.employee import EmployeeRepository
 from app.schema.employee import Employee, EmployeeCreate
+from app.service.employee import EmployeeService
 
 DUMMY_EMPLOYEE = Employee(
     id=UUID("00000000-0000-0000-0000-000000000001"),
     first_name="John",
     last_name="Doe",
+    timezone="Europe/Paris",
+    team_id=None,
 )
 
 
@@ -18,9 +21,10 @@ class TestEmployeeIsolatedRoute(unittest.TestCase):
         self.session = MagicMock()
         self.repository = EmployeeRepository
 
-    @patch.object(EmployeeRepository, "create", return_value=DUMMY_EMPLOYEE)
+    @patch.object(EmployeeService, "create_with_balance", return_value=DUMMY_EMPLOYEE)
     def test_create_employee(self, create: MagicMock):
-        employee = EmployeeCreate(first_name="John", last_name="Doe")
+        employee = EmployeeCreate(
+            first_name=DUMMY_EMPLOYEE.first_name, last_name=DUMMY_EMPLOYEE.last_name
+        )
         response = create_employee(session=self.session, employee=employee)
-        create.assert_called_once_with(session=self.session, obj_in=employee.dict())
         assert response == DUMMY_EMPLOYEE
