d44cc8559e466a2c72806367e35bed25084cc739
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 12:29:57 +0100

    added vacation type in overlap + cleaned and added some tests

--- app/repository/vacation.py
+++ app/repository/vacation.py
@@ -5,18 +5,26 @@ from sqlalchemy.orm import Session
 
 from app.model import VacationModel
 from app.repository.base import BaseRepository
+from app.schema.vacation import VacationCreate
 
 
 class _VacationRepository(BaseRepository[VacationModel]):
     def get_overlapping_vacations(
-        self, session: Session, employee_id: UUID, start_date: date, end_date: date
+        self, session: Session, vacation: VacationCreate
     ) -> list[VacationModel]:
+        """Returns a list of vacations that overlap with the given vacation."""
         return self.get_many(
             session,
-            self.model.employee_id == employee_id,
-            self.model.start_date <= end_date,
-            self.model.end_date >= start_date,
+            self.model.employee_id == vacation.employee_id,
+            self.model.type == vacation.type,
+            self.model.start_date <= vacation.end_date,
+            self.model.end_date >= vacation.start_date,
         )
+
+    def merge_vacations(
+        self, session: Session, overlapping_vacations: list[VacationModel]
+    ) -> None:
+        ...
 
 
 VacationRepository = _VacationRepository(model=VacationModel)
--- tests/test_repository_vacation.py
+++ tests/test_repository_vacation.py
@@ -1,6 +1,5 @@
 import unittest
 from datetime import date
-from unittest.mock import MagicMock, patch
 from uuid import UUID
 
 from sqlalchemy import create_engine
@@ -9,14 +8,14 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from app.model import VacationModel
 from app.model.base import BaseModel
 from app.repository.vacation import VacationRepository
-from app.schema.vacation import Vacation, VacationCreate, VacationType
+from app.schema.vacation import VacationCreate, VacationType
 
 
 def get_test_db() -> scoped_session:
     engine = create_engine("sqlite:///:memory:")
     session = sessionmaker(bind=engine)
     scoped = scoped_session(session)
-    BaseModel.metadata.create_all(engine)
+    BaseModel.metadata.create_all(engine)  # type: ignore
     return scoped
 
 
@@ -24,6 +23,9 @@ class TestVacationRepository(unittest.TestCase):
     def setUp(self):
         self.session = get_test_db()
         self.repository = VacationModel
+
+    def tearDown(self):
+        BaseModel.metadata.drop_all(self.session.bind)  # type: ignore
 
     def test_get_overlapping_vacations(self):
         vacation_1 = VacationRepository.create(
@@ -47,12 +49,66 @@ class TestVacationRepository(unittest.TestCase):
         # the vacations overlap on 2021-01-03 to 2021-01-05
         overlapping_vacations = VacationRepository.get_overlapping_vacations(
             self.session,
-            employee_id=UUID("00000000-0000-0000-0000-000000000000"),
-            start_date=date(2021, 1, 3),
-            end_date=date(2021, 1, 5),
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 3),
+                end_date=date(2021, 1, 7),
+                type=VacationType.PAID,
+            ),
         )
         self.assertEqual(len(overlapping_vacations), 2)
 
         # check that the overlapping vacations are the ones we expect
         self.assertEqual(overlapping_vacations[0].id, vacation_1.id)
         self.assertEqual(overlapping_vacations[1].id, vacation_2.id)
+
+    def test_get_overlapping_vacations_with_no_overlap(self):
+        VacationRepository.create(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 1),
+                end_date=date(2021, 1, 5),
+                type=VacationType.PAID,
+            ).dict(),
+        )
+        VacationRepository.create(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 6),
+                end_date=date(2021, 1, 7),
+                type=VacationType.PAID,
+            ).dict(),
+        )
+        overlapping_vacations = VacationRepository.get_overlapping_vacations(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 2, 3),
+                end_date=date(2021, 2, 7),
+                type=VacationType.PAID,
+            ),
+        )
+        self.assertEqual(len(overlapping_vacations), 0)
+
+    def test_get_overlapping_vacations_only_same_type(self):
+        VacationRepository.create(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 1),
+                end_date=date(2021, 1, 5),
+                type=VacationType.PAID,
+            ).dict(),
+        )
+        overlapping_vacations = VacationRepository.get_overlapping_vacations(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 3),
+                end_date=date(2021, 1, 7),
+                type=VacationType.UNPAID,
+            ),
+        )
+        self.assertEqual(len(overlapping_vacations), 0)
