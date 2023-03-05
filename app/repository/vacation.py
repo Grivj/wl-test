fda990890ee1333c8bf906d4320669f5a4a1131f
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 11:44:27 +0100

    added method to fetch overlapping vacations

--- app/repository/vacation.py
+++ app/repository/vacation.py
@@ -1,9 +1,22 @@
+from datetime import date
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
 from app.model import VacationModel
 from app.repository.base import BaseRepository
 
 
 class _VacationRepository(BaseRepository[VacationModel]):
-    ...
+    def get_overlapping_vacations(
+        self, session: Session, employee_id: UUID, start_date: date, end_date: date
+    ) -> list[VacationModel]:
+        return self.get_many(
+            session,
+            self.model.employee_id == employee_id,
+            self.model.start_date <= end_date,
+            self.model.end_date >= start_date,
+        )
 
 
 VacationRepository = _VacationRepository(model=VacationModel)
