7b33ac599859a3b592f2473ebc48b436544f7a9f
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 18:13:09 +0100

    added tests for get employees in vacation

--- tests/test_service_vacation.py
+++ tests/test_service_vacation.py
@@ -3,6 +3,7 @@ from datetime import date
 from uuid import UUID
 
 from app.model import VacationModel
+from app.repository.employee import EmployeeRepository
 from app.repository.vacation import VacationRepository
 from app.schema.vacation import VacationCreate, VacationType
 from app.service.vacation import VacationService
@@ -204,3 +205,136 @@ class TestVacationService(unittest.TestCase):
         self.assertEqual(updated_vacation.start_date, date(2021, 1, 2))
         self.assertEqual(updated_vacation.end_date, date(2021, 1, 7))
         self.assertEqual(updated_vacation.type, VacationType.PAID)
+
+    def test_get_employees_in_vacation(self):
+        # create some employees
+        jerome = EmployeeRepository.create(
+            self.session,
+            {
+                "id": UUID("00000000-0000-0000-0000-000000000000"),
+                "first_name": "Jerome",
+                "last_name": "Powell",
+            },
+        )
+        jim = EmployeeRepository.create(
+            self.session,
+            {
+                "id": UUID("00000000-0000-0000-0000-000000000001"),
+                "first_name": "Jim",
+                "last_name": "Cramer",
+            },
+        )
+        elisabeth = EmployeeRepository.create(
+            self.session,
+            {
+                "id": UUID("00000000-0000-0000-0000-000000000002"),
+                "first_name": "Elisabeth",
+                "last_name": "Warren",
+            },
+        )
+
+        # create some vacations
+        self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=jerome.id,
+                start_date=date(2021, 1, 1),
+                end_date=date(2021, 1, 3),
+                type=VacationType.PAID,
+            ),
+        )
+        self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=jerome.id,
+                start_date=date(2021, 1, 5),
+                end_date=date(2021, 1, 7),
+                type=VacationType.PAID,
+            ),
+        )
+        self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=jim.id,
+                start_date=date(2021, 1, 3),
+                end_date=date(2021, 1, 4),
+                type=VacationType.UNPAID,
+            ),
+        )
+        self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=elisabeth.id,
+                start_date=date(2021, 1, 11),
+                end_date=date(2021, 1, 20),
+                type=VacationType.PAID,
+            ),
+        )
+
+        # get the employees in vacation
+        # elisabeth is not in vacation in this period
+        employees = self.service.get_employees_in_vacation(
+            self.session, date(2021, 1, 1), date(2021, 1, 10)
+        )
+
+        # assert the employees were returned
+        self.assertIsInstance(employees, set)
+        self.assertEqual(len(employees), 2)
+        self.assertEqual(employees, {jerome, jim})
+
+    def test_get_employees_in_vacation_by_type(self):
+        # create some employees
+        jerome = EmployeeRepository.create(
+            self.session,
+            {
+                "id": UUID("00000000-0000-0000-0000-000000000000"),
+                "first_name": "Jerome",
+                "last_name": "Powell",
+            },
+        )
+        jim = EmployeeRepository.create(
+            self.session,
+            {
+                "id": UUID("00000000-0000-0000-0000-000000000001"),
+                "first_name": "Jim",
+                "last_name": "Cramer",
+            },
+        )
+
+        # create some vacations
+        self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=jerome.id,
+                start_date=date(2021, 1, 1),
+                end_date=date(2021, 1, 3),
+                type=VacationType.PAID,
+            ),
+        )
+        self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=jerome.id,
+                start_date=date(2021, 1, 5),
+                end_date=date(2021, 1, 7),
+                type=VacationType.PAID,
+            ),
+        )
+        self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=jim.id,
+                start_date=date(2021, 1, 3),
+                end_date=date(2021, 1, 4),
+                type=VacationType.UNPAID,
+            ),
+        )
+
+        # get the employees in paid vacation
+        employees = self.service.get_employees_in_vacation(
+            self.session, date(2021, 1, 1), date(2021, 1, 10), VacationType.UNPAID
+        )
+
+        # assert the employees were returned
+        self.assertEqual(len(employees), 1)
+        self.assertEqual(employees, {jim})
