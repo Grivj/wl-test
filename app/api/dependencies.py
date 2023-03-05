6e7d3c3e0d4d8a863350af79297276476d54598f
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 16:45:04 +0100

    added routes for handling vacations

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -4,9 +4,10 @@ from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
-from app.model import EmployeeModel, TeamModel
+from app.model import EmployeeModel, TeamModel, VacationModel
 from app.repository.employee import EmployeeRepository
 from app.repository.team import TeamRepository
+from app.repository.vacation import VacationRepository
 
 
 def get_employee_by_id(
@@ -23,3 +24,12 @@ def get_team_by_id(db: Session = Depends(get_db), *, team_id: UUID) -> TeamModel
     if not (team := TeamRepository.get_by_id(db, team_id)):
         raise HTTPException(status_code=404, detail="Team not found")
     return team
+
+
+def get_vacation_by_id(
+    db: Session = Depends(get_db), *, vacation_id: UUID
+) -> VacationModel:
+    """Returns a vacation by id or raises an HTTPException if not found."""
+    if not (vacation := VacationRepository.get_by_id(db, vacation_id)):
+        raise HTTPException(status_code=404, detail="Vacation not found")
+    return vacation
--- app/api/routes/vacation.py
+++ app/api/routes/vacation.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from app.api.dependencies import get_employee_by_id
+from app.api.dependencies import get_employee_by_id, get_vacation_by_id
 from app.db.session import get_db
-from app.model import EmployeeModel
+from app.model import EmployeeModel, VacationModel
 from app.repository.vacation import VacationRepository
-from app.schema.vacation import Vacation
+from app.schema.vacation import Vacation, VacationCreate
+from app.service.vacation import VacationService
 
 router = APIRouter()
 
@@ -18,3 +19,45 @@ def get_employee_vacations(
         Vacation.from_orm(model)
         for model in VacationRepository.get_many(db, employee_id=employee.id)
     ]
+
+
+@router.post("/{employee_id}", response_model=Vacation)
+def create_employee_vacation(
+    *,
+    db: Session = Depends(get_db),
+    employee: EmployeeModel = Depends(get_employee_by_id),
+    vacation: VacationCreate,
+) -> Vacation:
+    # check that the employee id in the path matches the employee id in the body
+    if employee.id != vacation.employee_id:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Employee id in path ({employee.id}) does not match employee id in body ({vacation.employee_id})",
+        )
+    try:
+        return Vacation.from_orm(VacationService.create(db, vacation))
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+
+@router.delete("/{employee_id}/vacations/{vacation_id}", status_code=status.HTTP_200_OK)
+async def delete_vacation(
+    *,
+    db: Session = Depends(get_db),
+    employee: EmployeeModel = Depends(get_employee_by_id),
+    vacation: VacationModel = Depends(get_vacation_by_id),
+):
+    VacationRepository.delete(db, vacation)
+    return {"message": "Vacation deleted"}
+
+
+@router.put("/{employee_id}/vacations/{vacation_id}", status_code=status.HTTP_200_OK)
+def update_vacation(
+    *,
+    db: Session = Depends(get_db),
+    employee: EmployeeModel = Depends(get_employee_by_id),
+    vacation: VacationModel = Depends(get_vacation_by_id),
+    vacation_update: VacationCreate,
+):
+    VacationService.update(db, vacation, vacation_update)
+    return {"message": "Vacation updated"}
