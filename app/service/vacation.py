c2036e45bf98e7127b52664cb2a44f4a2d745b21
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 16:49:40 +0100

    added method to update vacation + minor refactoring

--- app/service/vacation.py
+++ app/service/vacation.py
@@ -6,11 +6,11 @@ from app.model.vacation import VacationModel
 from app.repository.vacation import VacationRepository
 from app.schema.vacation import VacationCreate
 
-from .vacation_validators import VacationValidator
+from .vacation_validators import OverlappingVacationTypeValidator, VacationValidator
 
 
 @dataclass
-class VacationService:
+class _VacationService:
     validators: list[VacationValidator]
     repository = VacationRepository
 
@@ -24,22 +24,59 @@ class VacationService:
                 session, vacation=vacation, overlapping_vacations=overlapping_vacations
             )
         if overlapping_vacations:
-            return self.merge_vacations(session, vacation, overlapping_vacations)
+            return self.handle_overlapping_vacations(
+                session, vacation, overlapping_vacations
+            )
         return self.repository.create(session, vacation.dict())
+
+    def update(
+        self,
+        session: Session,
+        old_vacation: VacationModel,
+        new_vacation: VacationCreate,
+    ) -> VacationModel:
+        overlapping_vacations = self.repository.get_overlapping_vacations(
+            session, new_vacation
+        )
+        # remove the old vacation from the overlapping vacations
+        overlapping_vacations = [
+            v for v in overlapping_vacations if v.id != old_vacation.id
+        ]
+
+        for validator in self.validators:
+            validator.validate(
+                session,
+                vacation=new_vacation,
+                overlapping_vacations=overlapping_vacations,
+            )
+        if overlapping_vacations:
+            return self.handle_overlapping_vacations(
+                session, new_vacation, overlapping_vacations
+            )
+        return self.repository.update(session, VacationModel(**new_vacation.dict()))
+
+    def handle_overlapping_vacations(
+        self,
+        session: Session,
+        vacation: VacationCreate,
+        overlapping_vacations: list[VacationModel],
+    ) -> VacationModel:
+        merged_vacation = self.merge_vacations(session, vacation, overlapping_vacations)
+        self.repository.delete_many(session, overlapping_vacations)
+        return self.repository.create(session, merged_vacation.dict())
 
     def merge_vacations(
         self,
         session: Session,
         vacation: VacationCreate,
         overlapping_vacations: list[VacationModel],
-    ) -> VacationModel:
+    ) -> VacationCreate:
         # merge the overlapping vacations into the new vacation
         start_date = min(v.start_date for v in overlapping_vacations)
         end_date = max(v.end_date for v in overlapping_vacations)
         vacation.start_date = min(vacation.start_date, start_date)
         vacation.end_date = max(vacation.end_date, end_date)
-        # delete the overlapping vacations
-        for v in overlapping_vacations:
-            self.repository.delete(session, v)
-        # create the new vacation
-        return self.repository.create(session, vacation.dict())
+        return vacation
+
+
+VacationService = _VacationService(validators=[OverlappingVacationTypeValidator])
--- tests/test_service_vacation.py
+++ tests/test_service_vacation.py
@@ -6,18 +6,13 @@ from app.model import VacationModel
 from app.repository.vacation import VacationRepository
 from app.schema.vacation import VacationCreate, VacationType
 from app.service.vacation import VacationService
-from app.service.vacation_validators import (
-    OverlappingVacationTypeValidator,
-    VacationValidator,
-)
 from tests.utils import get_test_db
 
 
 class TestVacationService(unittest.TestCase):
     def setUp(self):
         self.session = get_test_db()
-        self.validators: list[VacationValidator] = [OverlappingVacationTypeValidator]
-        self.service = VacationService(self.validators)
+        self.service = VacationService
 
     def test_create_vacation(self):
         vacation_data = VacationCreate(
@@ -73,6 +68,33 @@ class TestVacationService(unittest.TestCase):
         self.assertEqual(vacation.end_date, date(2021, 1, 7))
         self.assertEqual(vacation.type, VacationType.PAID)
 
+    def test_create_vacation_with_contiguous_vacations(self):
+        VacationRepository.create(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 1),
+                end_date=date(2021, 1, 5),
+                type=VacationType.PAID,
+            ).dict(),
+        )
+        vacation_data = VacationCreate(
+            employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+            start_date=date(2021, 1, 6),
+            end_date=date(2021, 1, 10),
+            type=VacationType.PAID,
+        )
+
+        vacation = self.service.create(self.session, vacation_data)
+
+        self.assertIsInstance(vacation, VacationModel)
+        self.assertEqual(
+            vacation.employee_id, UUID("00000000-0000-0000-0000-000000000000")
+        )
+        self.assertEqual(vacation.start_date, date(2021, 1, 1))
+        self.assertEqual(vacation.end_date, date(2021, 1, 10))
+        self.assertEqual(vacation.type, VacationType.PAID)
+
     def test_create_vacation_with_overlapping_vacations_but_wrong_type(self):
         VacationRepository.create(
             self.session,
@@ -103,3 +125,82 @@ class TestVacationService(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             self.service.create(self.session, vacation_data)
+
+    def test_update_vacation(self):
+        # create a vacation
+        vacation = self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 1),
+                end_date=date(2021, 1, 5),
+                type=VacationType.PAID,
+            ),
+        )
+        self.assertIsInstance(vacation, VacationModel)
+
+        # update the vacation
+        updated_vacation = self.service.update(
+            self.session,
+            vacation,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 2),
+                end_date=date(2021, 1, 6),
+                type=VacationType.PAID,
+            ),
+        )
+        self.assertIsInstance(updated_vacation, VacationModel)
+
+        # assert the updates were successful
+        self.assertEqual(
+            updated_vacation.employee_id, UUID("00000000-0000-0000-0000-000000000000")
+        )
+        self.assertEqual(updated_vacation.start_date, date(2021, 1, 2))
+        self.assertEqual(updated_vacation.end_date, date(2021, 1, 6))
+        self.assertEqual(updated_vacation.type, VacationType.PAID)
+
+    def test_update_vacation_with_overlapping_vacations(self):
+        # create a vacation
+        vacation = self.service.create(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 1),
+                end_date=date(2021, 1, 5),
+                type=VacationType.PAID,
+            ),
+        )
+        self.assertIsInstance(vacation, VacationModel)
+
+        # create another vacation
+        VacationRepository.create(
+            self.session,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 3),
+                end_date=date(2021, 1, 7),
+                type=VacationType.PAID,
+            ).dict(),
+        )
+
+        # update the vacation
+        updated_vacation = self.service.update(
+            self.session,
+            vacation,
+            VacationCreate(
+                employee_id=UUID("00000000-0000-0000-0000-000000000000"),
+                start_date=date(2021, 1, 2),
+                end_date=date(2021, 1, 6),
+                type=VacationType.PAID,
+            ),
+        )
+        self.assertIsInstance(updated_vacation, VacationModel)
+
+        # assert the updates were successful
+        self.assertEqual(
+            updated_vacation.employee_id, UUID("00000000-0000-0000-0000-000000000000")
+        )
+        self.assertEqual(updated_vacation.start_date, date(2021, 1, 2))
+        self.assertEqual(updated_vacation.end_date, date(2021, 1, 7))
+        self.assertEqual(updated_vacation.type, VacationType.PAID)
