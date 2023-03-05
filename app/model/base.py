1b46ed177cb5faddd0d2907b7add50c875f63344
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 14:17:28 +0100

    added cache for custom uuid

--- app/model/base.py
+++ app/model/base.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import as_declarative  # type: ignore[import]
 
 class CustomUUID(types.TypeDecorator[uid.UUID]):
     impl = types.CHAR
+    cache_ok = True
 
     def process_bind_param(self, value: uid.UUID | None, _):  # type: ignore[override]
         return None if value is None else str(value)
