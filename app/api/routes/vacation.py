1dbbf7a7ed968b8397abfafcf84a1ba0dfe18860
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 19:15:35 +0100

    added route to compare two employees vacations

--- app/api/routes/vacation.py
+++ app/api/routes/vacation.py
@@ -1,4 +1,5 @@
 from datetime import date
+from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
@@ -6,10 +7,12 @@ from sqlalchemy.orm import Session
 from app.api.dependencies import get_employee_by_id, get_vacation_by_id
 from app.db.session import get_db
 from app.model import EmployeeModel, VacationModel
+from app.repository.employee import EmployeeRepository
 from app.repository.vacation import VacationRepository
 from app.schema.employee import Employee
 from app.schema.vacation import Vacation, VacationCreate, VacationType
 from app.service.vacation import VacationService
+from app.service.vacation_comparison import VacationComparisonService
 
 router = APIRouter()
 
@@ -28,6 +31,28 @@ def search_employees_by_period(
             db, start_date, end_date, type
         )
     ]
+
+
+@router.get("/compare_employees_vacations", response_model=list[date])
+def compare_employees_vacations(
+    *,
+    db: Session = Depends(get_db),
+    employee_1_id: UUID,
+    employee_2_id: UUID,
+    start_date: date,
+    end_date: date,
+) -> list[date]:
+    if not (employee_1 := EmployeeRepository.get_by_id(db, employee_1_id)):
+        raise HTTPException(
+            status_code=404, detail=f"Employee {employee_1_id} not found"
+        )
+    if not (employee_2 := EmployeeRepository.get_by_id(db, employee_2_id)):
+        raise HTTPException(
+            status_code=404, detail=f"Employee {employee_2_id} not found"
+        )
+    return VacationComparisonService.compare_employees_vacations(
+        db, employee_1, employee_2, start_date, end_date
+    )
 
 
 @router.get("/{employee_id}", response_model=list[Vacation])
