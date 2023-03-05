92fd9c5655d154ee7782c23fff4dcc46a4c4a764
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-05 14:31:42 +0100

    renamed validator for vacation type + added merge method in service

--- app/service/vacation.py
+++ app/service/vacation.py
@@ -1,32 +1,45 @@
 from dataclasses import dataclass
 
-from workalendar.core import Calendar
-from workalendar.europe import France
+from sqlalchemy.orm import Session
 
 from app.model.vacation import VacationModel
+from app.repository.vacation import VacationRepository
+from app.schema.vacation import VacationCreate
 
 from .vacation_validators import VacationValidator
 
 
-def get_calendar_for_tz(tz: str) -> Calendar:
-    """
-    Return a calendar for the given timezone.
-    For now, only France is supported.
-    """
-    match tz:
-        case "Europe/Paris":
-            return France()
-        case _:
-            raise ValueError(f"Timezone {tz} not supported yet")
-
-
 @dataclass
-class VacationCreationService:
+class VacationService:
     validators: list[VacationValidator]
+    repository = VacationRepository
 
-    def create_vacation(self, vacation: VacationModel) -> VacationModel:
+    def create(self, session: Session, vacation: VacationCreate) -> VacationModel:
+        overlapping_vacations = self.repository.get_overlapping_vacations(
+            session, vacation
+        )
+
         for validator in self.validators:
-            if not validator.validate(vacation):
-                raise ValueError("Vacation is not valid")
+            validator.validate(
+                session, vacation=vacation, overlapping_vacations=overlapping_vacations
+            )
+        if overlapping_vacations:
+            return self.merge_vacations(session, vacation, overlapping_vacations)
+        return self.repository.create(session, vacation.dict())
 
-        return vacation
+    def merge_vacations(
+        self,
+        session: Session,
+        vacation: VacationCreate,
+        overlapping_vacations: list[VacationModel],
+    ) -> VacationModel:
+        # merge the overlapping vacations into the new vacation
+        start_date = min(v.start_date for v in overlapping_vacations)
+        end_date = max(v.end_date for v in overlapping_vacations)
+        vacation.start_date = min(vacation.start_date, start_date)
+        vacation.end_date = max(vacation.end_date, end_date)
+        # delete the overlapping vacations
+        for v in overlapping_vacations:
+            self.repository.delete(session, v)
+        # create the new vacation
+        return self.repository.create(session, vacation.dict())
--- app/service/vacation_validators.py
+++ app/service/vacation_validators.py
@@ -1,13 +1,32 @@
 from typing import Protocol
 
+from workalendar.core import Calendar
+from workalendar.europe import France
+
 from app.model.vacation import VacationModel
+from app.schema.vacation import VacationCreate
+
+
+def get_calendar_for_tz(tz: str) -> Calendar:
+    """
+    Return a calendar for the given timezone.
+    For now, only France is supported.
+    """
+    match tz:
+        case "Europe/Paris":
+            return France()
+        case _:
+            raise ValueError(f"Timezone {tz} not supported yet")
 
 
 class VacationValidator(Protocol):
-    def validate(self, vacation: VacationModel) -> bool:
+    def validate(self, *args: ..., **kwargs: ...) -> None:
         ...
 
 
-class OverlappingVacationValidator:
-    def validate(self, vacation: VacationModel) -> bool:
-        ...
+class OverlappingVacationTypeValidator:
+    def validate(self, *args: ..., **kwargs: ...) -> None:
+        vacation: VacationCreate = kwargs["vacation"]
+        overlapping_vacations: list[VacationModel] = kwargs["overlapping_vacations"]
+        if any(v.type != vacation.type for v in overlapping_vacations):
+            raise ValueError("Vacations are of different types")
