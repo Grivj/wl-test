392d5263a2c535d05f3e0e6f34df6b3c69b0e46a
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-07 14:09:47 +0100

    replaced session mock by test db + updated tests

--- tests/test_repository_team.py
+++ tests/test_repository_team.py
@@ -1,35 +1,42 @@
 import unittest
-from unittest.mock import MagicMock, patch
 from uuid import UUID
 
 from app.model import TeamModel
 from app.repository.team import TeamRepository
 from app.schema.employee import Employee
-from app.schema.team import Team
-
-DUMMY_TEAM = Team(id=UUID("00000000-0000-0000-0000-000000000001"), name="TeamA")
-DUMMY_TEAM_MODEL = TeamModel(**DUMMY_TEAM.dict())
-DUMMY_EMPLOYEE = Employee(
-    id=UUID("00000000-0000-0000-0000-000000000001"),
-    first_name="John",
-    last_name="Doe",
-)
+from app.schema.team import TeamCreate
+from tests.utils import get_test_db
 
 
 class TestTeamRepository(unittest.TestCase):
     def setUp(self):
-        self.session = MagicMock()
-        self.repository = TeamRepository
+        self.session = get_test_db()
+        self.repository = TeamRepository(TeamModel)
 
-    @patch.object(TeamRepository, "create", return_value=DUMMY_TEAM_MODEL)
-    def test_create_team(self, create: MagicMock):
-        obj_in = {"name": DUMMY_TEAM.name}
-        response = self.repository.create(session=self.session, obj_in=obj_in)
-        create.assert_called_once_with(session=self.session, obj_in=obj_in)
-        assert response == DUMMY_TEAM_MODEL
+        self.dummy_team_schema = TeamCreate(name="TeamA")
+        self.dummy_team_model = TeamModel(**self.dummy_team_schema.dict())
+        self.dummy_employee_schema = Employee(
+            id=UUID("00000000-0000-0000-0000-000000000001"),
+            first_name="John",
+            last_name="Doe",
+        )
 
-    @patch.object(TeamRepository, "get", return_value=DUMMY_TEAM_MODEL)
-    def test_get_team(self, get: MagicMock):
-        response = self.repository.get(session=self.session, id=1)
-        get.assert_called_once_with(session=self.session, id=1)
-        assert response == DUMMY_TEAM_MODEL
+    def test_create_team(self):
+        # test successful creation
+        team = self.repository.create_team(self.session, self.dummy_team_schema)
+        self.assertEqual(team.name, self.dummy_team_schema.name)
+
+        # test get team by id
+        team = self.repository.get_team_by_id(self.session, team.id)
+        self.assertIsNotNone(team)
+        assert team is not None
+        self.assertEqual(team.name, self.dummy_team_schema.name)
+
+    def test_get_team_by_name(self):
+        # create team
+        team = self.repository.create_team(self.session, self.dummy_team_schema)
+
+        # fetch team by name
+        team = self.repository.get_team_by_name(self.session, team.name)
+        assert team is not None
+        self.assertEqual(team.name, self.dummy_team_schema.name)
