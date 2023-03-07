45d2ecb21da6bbf637f99bb8b0b4466b082e151b
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-07 15:33:06 +0100

    relative imports on repos

---
+++ app/repository/__init__.py
@@ -0,0 +1,4 @@
+from .balance import BalanceRepository  # type: ignore
+from .employee import EmployeeRepository  # type: ignore
+from .team import TeamRepository  # type: ignore
+from .vacation import VacationRepository  # type: ignore
