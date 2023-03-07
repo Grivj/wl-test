42ddca483fc9e304c3407f19d606e79e2c1dc7b6
Jordan Griveau <jgriveau@yahoo.fr>
2023-03-07 15:40:06 +0100

    added vacation comparison service in relative imports

--- app/service/__init__.py
+++ app/service/__init__.py
@@ -1,2 +1,3 @@
 from .employee import EmployeeService  # type: ignore
 from .vacation import VacationService  # type: ignore
+from .vacation_comparison import VacationComparisonService  # type: ignore
